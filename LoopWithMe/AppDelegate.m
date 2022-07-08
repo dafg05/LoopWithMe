dd109ac9215e26b6306b338f268d694a4f2228f3
dafg05 <danialefloresg@gmail.com>
2022-07-08 11:15:01 -0500

    Connect to Back4App

--- LoopWithMe/AppDelegate.m
+++ LoopWithMe/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -15,7 +16,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"Keys" ofType: @"plist"];
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        configuration.applicationId = [dict objectForKey: @"appID"];
+        configuration.clientKey = [dict objectForKey: @"clientKey"];
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+    [Parse initializeWithConfiguration:config];
     return YES;
 }
 
