7862966a3daea984f1251317a1be7908d42aa487
dafg05 <danialefloresg@gmail.com>
2022-07-10 11:48:41 -0700

    Restructured project directories

---
+++ LoopWithMe/Models/TimeSignature.m
@@ -0,0 +1,19 @@
+//
+//  TimeSignature.m
+//  LoopWithMe
+//
+//  Created by Daniel Flores Garcia on 7/10/22.
+//
+
+#import "TimeSignature.h"
+
+@implementation TimeSignature
+
+@dynamic numerator;
+@dynamic denominator;
+
++ (nonnull NSString *)parseClassName {
+    return @"TimeSignature";
+}
+
+@end
