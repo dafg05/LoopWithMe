7bf1e2124415e0d92b325426691cd68564af57ca
dafg05 <danialefloresg@gmail.com>
2022-07-10 19:22:21 -0700

    Implemented login and sign up

--- LoopWithMe/ViewControllers/LoginVC.m
+++ LoopWithMe/ViewControllers/LoginVC.m
@@ -6,8 +6,12 @@
 //
 
 #import "LoginVC.h"
+#import "Parse/Parse.h"
 
 @interface LoginVC ()
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -15,7 +19,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.errorLabel.text = @"";
+    // Set up custom placeholders
+    self.usernameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Username" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    self.passwordField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Password" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+}
+
+- (IBAction)didTapLogin:(id)sender {
+    [self loginUser];
+}
+
+- (void)loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+            // TODO: remove hardcoding
+            if ([error.localizedDescription isEqualToString:@"username/email is required."]){
+                self.errorLabel.text = @"username is required.";
+            }
+            else{
+                self.errorLabel.text = error.localizedDescription;
+            }
+            
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            [self performSegueWithIdentifier:@"LoginSegue" sender:nil];
+        }
+    }];
 }
 
 /*
--- LoopWithMe/ViewControllers/SignUpVC.m
+++ LoopWithMe/ViewControllers/SignUpVC.m
@@ -6,8 +6,17 @@
 //
 
 #import "SignUpVC.h"
+#import "Parse/Parse.h"
 
 @interface SignUpVC ()
+
+@property (weak, nonatomic) IBOutlet UITextField *givenNameField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPWField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+
 
 @end
 
@@ -15,7 +24,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.errorLabel.text = @"";
+    // Set up custom placeholders
+    self.givenNameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Enter your name" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    self.usernameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Username" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    self.passwordField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Password" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    self.confirmPWField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Confirm password" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    self.emailField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Email" attributes:@{NSForegroundColorAttributeName: UIColor.systemGrayColor}];
+    
+}
+
+- (IBAction)didTapSignUp:(id)sender {
+    [self registerUser];
+}
+
+
+- (void)registerUser {
+    
+    if (self.checkForEmptyFields){
+        self.errorLabel.text = @"One or more fields are empty.";
+        return;
+    }
+        
+    if (![self.passwordField.text isEqualToString:self.confirmPWField.text]){
+        NSLog(@"Passwords don't match!");
+        self.errorLabel.text = @"Passwords don't match.";
+        return;
+    }
+    
+    PFUser *newUser = [PFUser user];
+
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    newUser.email = self.emailField.text;
+    newUser[@"givenName"] = self.givenNameField.text;
+
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+            self.errorLabel.text = error.localizedDescription;
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"SignUpSegue" sender:nil];
+        }
+    }];
+}
+
+- (BOOL) checkForEmptyFields{
+    BOOL givenNameEmpty = ([self.givenNameField.text isEqualToString:@""]) ? TRUE : FALSE;
+    BOOL usernameEmpty = ([self.usernameField.text isEqualToString:@""]) ? TRUE : FALSE;
+    BOOL emailEmpty = ([self.emailField.text isEqualToString:@""]) ? TRUE : FALSE;
+    BOOL passwordEmpty = ([self.passwordField.text isEqualToString:@""]) ? TRUE : FALSE;
+    BOOL confirmPWEmpty = ([self.confirmPWField.text isEqualToString:@""]) ? TRUE : FALSE;
+    
+    return (givenNameEmpty || usernameEmpty || emailEmpty || passwordEmpty || confirmPWEmpty);
 }
 
 /*
