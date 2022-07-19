028c27ef7f3f2616ff7187e8fed9e52080c0fe00
dafg05 <danialefloresg@gmail.com>
2022-07-19 10:35:51 -0700

    Added setup instructions to README

--- LoopWithMe/ViewControllers/Authentication/LoginVC.m
+++ LoopWithMe/ViewControllers/Authentication/LoginVC.m
@@ -76,14 +76,6 @@
     SceneDelegate *sceneDelegate = (SceneDelegate * ) UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
     [sceneDelegate changeRootViewController:mainTBC];
 }
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# LoopWithMe
+
 [Link to project plan](https://github.com/dafg05/LoopWithMePlanning)
+
+Before building:
+
+`pod install`
+
+Add appID and clientKey to Keys.plist (if in possesion).
