ae54b36cdc3bc003f8cb14ee3bee78598710b182
dafg05 <danialefloresg@gmail.com>
2022-07-20 13:35:58 -0700

    Added function for querying loop posts

--- LoopWithMe/ViewControllers/Home/HomeVC.m
+++ LoopWithMe/ViewControllers/Home/HomeVC.m
@@ -7,10 +7,17 @@
 
 #import "HomeVC.h"
 #import "FeedCell.h"
+#import "PlayStopButton.h"
+#import "Parse/Parse.h"
 
 @interface HomeVC () <UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *feedTableView;
+@property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+@property (weak, nonatomic) IBOutlet PlayStopButton *playStopButton;
+@property (strong, nonatomic) NSMutableArray *loops;
 
 @end
 
@@ -19,10 +26,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.feedTableView.dataSource = self;
+    [self queryLoops];
+    
 }
 
-
-
+- (void)queryLoops {
+    PFQuery *query = [PFQuery queryWithClassName:@"Loop"];
+    [query orderByDescending:@"createdAt"];
+    query.limit = 20;
+    [query findObjectsInBackgroundWithBlock:^(NSArray *loops, NSError *error) {
+        if (loops != nil) {
+            self.loops = (NSMutableArray *)loops;
+            [self.feedTableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+}
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
     FeedCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FeedCell"];
